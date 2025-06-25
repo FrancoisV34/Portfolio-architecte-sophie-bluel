994ad181c2d37307c318c491004bf85d93f402e3
François <francois.vittecoq@osteopathe-eso.fr>
2025-06-25 21:43:34 +0200

    feat (HTML JS) HTML -> just space or line break
    JS -> create dynamic button and filter for the works

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -12,23 +12,133 @@ getWorks().then(data => {
 
             const imgWork = document.createElement("img");
             imgWork.src = data[i].imageUrl
-            console.log(imgWork)
 
             const figcaptionWork = document.createElement("figcaption")
             figcaptionWork.innerText = data[i].title;
-            console.log(figcaptionWork)
 
             const globalFig = document.createElement("figure")
-
-
 
             globalFig.appendChild(imgWork)
             globalFig.appendChild(figcaptionWork)
 
             sectionWork.appendChild(globalFig);
         }
+    }
+})
+
+getWorks().then(data => {
+    if (data.length > 0) {
+        const divFiltres = document.querySelector(".filtres");
+
+        //Bouton de "réactualisation" des filtres => apparition de tous les projets
+
+        const tousBtn = document.createElement("button")
+        tousBtn.id = "Tous";
+        tousBtn.innerText = "Tous";
+        tousBtn.addEventListener("click", function () {
+            const sectionWork = document.querySelector(".gallery")
+            sectionWork.innerHTML = "";
+
+            data.forEach(work => {
+                const imgWork = document.createElement("img");
+                imgWork.src = work.imageUrl
+
+                const figcaptionWork = document.createElement("figcaption")
+                figcaptionWork.innerText = work.title;
+
+                const globalFig = document.createElement("figure")
+
+                globalFig.appendChild(imgWork)
+                globalFig.appendChild(figcaptionWork)
+
+                sectionWork.appendChild(globalFig);
+            })
+        })
+
+        // Boutons pour select only "Objets"
+
+        const objetsBtn = document.createElement("button")
+        objetsBtn.id = "Objets";
+        objetsBtn.innerText = "Objets";
+        objetsBtn.addEventListener("click", function () {
+            const objWorks = data.filter(data => data.categoryId === 1)
+            const sectionWork = document.querySelector(".gallery")
+            sectionWork.innerHTML = "";
+
+            objWorks.forEach(work => {
+                const figure = document.createElement("figure");
+
+                const img = document.createElement("img");
+                img.src = work.imageUrl;
+
+                const caption = document.createElement("figcaption");
+                caption.innerText = work.title;
+
+                figure.appendChild(img);
+                figure.appendChild(caption);
+                sectionWork.appendChild(figure);
+            })
+        })
+
+        // Bouton pour faire apparaitre only "Appartements"
+
+        const appartBtn = document.createElement("button")
+        appartBtn.id = "Appartements";
+        appartBtn.innerText = "Appartements";
+        appartBtn.addEventListener("click", function () {
+            const appartWorks = data.filter(data => data.categoryId === 2)
+            const sectionWork = document.querySelector(".gallery")
+            sectionWork.innerHTML = ""
+
+            appartWorks.forEach(work => {
+                const figure = document.createElement("figure");
+
+                const img = document.createElement("img");
+                img.src = work.imageUrl;
+
+                const caption = document.createElement("figcaption");
+                caption.innerText = work.title;
+
+                figure.appendChild(img);
+                figure.appendChild(caption);
+                sectionWork.appendChild(figure);
+            })
+
+        })
+
+        // Bouton only Hôtels & Restaurants
+
+        const hrBtn = document.createElement("button")
+        hrBtn.id = "Hotels&Restaurants";
+        hrBtn.innerText = "Hôtels & Restaurants";
+        hrBtn.addEventListener("click", function () {
+            const hrWorks = data.filter(data => data.categoryId === 3)
+            const sectionWork = document.querySelector(".gallery")
+            sectionWork.innerHTML = ""
+
+            hrWorks.forEach(work => {
+                const figure = document.createElement("figure");
+
+                const img = document.createElement("img");
+                img.src = work.imageUrl;
+
+                const caption = document.createElement("figcaption");
+                caption.innerText = work.title;
+
+                figure.appendChild(img);
+                figure.appendChild(caption);
+                sectionWork.appendChild(figure);
+            })
+
+        })
+
+        divFiltres.appendChild(tousBtn);
+        divFiltres.appendChild(objetsBtn);
+        divFiltres.appendChild(appartBtn);
+        divFiltres.appendChild(hrBtn);
 
     }
+
 })
 
 console.log("caca")
